c425af93ecf5a0fa7100e53e1a82dcd1d71b80d6
Thrive Pro <thrivepro.agency@gmail.com>
2025-04-04 12:29:22 +0300

    Advanced search working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
+
+  # Advanced search (POST request)
+  get "advanced_search", to: "home#advanced_search", as: :advanced_search
+  post "search_results", to: "home#search_results", as: :search_results
+
+  # Homepage
   root "home#index"
 end
